a9a8efea894d6b34543b03b1e390beb741dd61db
Kiet Ho <hoakiet98@gmail.com>
2024-01-20 17:01:21 -0500

    Add plugin in nextjs example

--- demos/nextjs/next.config.mjs
+++ demos/nextjs/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    swcPlugins: [["@swc/plugin-onlook-react", {}]],
+  },
+};
 
 export default nextConfig;
