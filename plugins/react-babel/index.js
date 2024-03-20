a4f9058ee10f4722e5ce253bdc8a118ac309a93a
Kiet Ho <hoakiet98@gmail.com>
2024-03-20 14:00:05 -0400

    Update Babel plugin

--- plugins/react-babel/index.js
+++ plugins/react-babel/index.js
@@ -1,39 +1,67 @@
-const { default: generate } = require('@babel/generator');
-const { parse } = require('@babel/parser');
-const traverse = require('@babel/traverse').default;
-const t = require('@babel/types');
-const { generateDataAttributeValue } = require("../shared/helpers.js");
+const babel = require("@babel/core");
+const fs = require("fs");
+const path = require("path");
+const generate = require("@babel/generator").default;
+const { parse } = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const t = require("@babel/types");
 const { DATA_ONLOOK_ID } = require("../shared/constants.js");
+const { generateDataAttributeValue } = require("../shared/helpers.js");
 
-module.exports = function babelPluginOnlook({ root = process.cwd(), absolute = false }) {
+const onlookBabelPlugin = ({ root = path.resolve('.'), absolute = false }) => {
   return {
     visitor: {
-      JSXOpeningElement(path, state) {
+      Program(path, state) {
         const filename = state.file.opts.filename;
-        const nodeModulesPath = `${root}/node_modules`;
+        const nodeModulesPath = path.resolve(root, "node_modules");
 
         // Ignore node_modules
         if (filename.startsWith(nodeModulesPath)) {
           return;
         }
 
-        const attributeValue = generateDataAttributeValue(
-          filename,
-          path.node.loc.start.line,
-          path.node.loc.end.line,
-          root,
-          absolute
-        );
+        let offset = 0;
+        try {
+          // Calculate offset from typescript preprocessing step
+          let data = fs.readFileSync(filename, "utf-8");
+          let originalLineNum = data.split("\n").length;
+          let postLineNum = path.node.loc.end.line;
+          offset = originalLineNum - postLineNum;
+        } catch (e) {
+          offset = 0;
+        }
 
-        // Create the custom attribute
-        const onlookAttribute = t.jSXAttribute(
-          t.jSXIdentifier(DATA_ONLOOK_ID),
-          t.stringLiteral(attributeValue)
-        );
+        traverse(path.node, {
+          JSXOpeningElement(jsxPath) {
+            const node = jsxPath.node;
 
-        // Append the attribute to the element
-        path.node.attributes.push(onlookAttribute);
-      },
-    },
+            // Calculate the line number for each element node
+            const lineStart = node.loc.start.line + offset;
+            const lineEnd = node.loc.end.line + offset;
+
+            // Generate the attribute value
+            const attributeValue = generateDataAttributeValue(
+              filename,
+              lineStart,
+              lineEnd,
+              lineEnd, // Assuming closing tag is on the same line
+              root,
+              absolute
+            );
+
+            // Create the JSX attribute
+            const jsxAttribute = t.jsxAttribute(
+              t.jsxIdentifier(DATA_ONLOOK_ID),
+              t.stringLiteral(attributeValue)
+            );
+
+            // Add the attribute to the opening element
+            jsxPath.node.attributes.push(jsxAttribute);
+          }
+        });
+      }
+    }
   };
 };
+
+module.exports = onlookBabelPlugin;
